129a1cc6284ce7bea95336717d0a394cbf878081
Amantay <amantay.sv@gmail.com>
2023-10-20 21:04:36 +0600

    localstorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Header } from './components/Header/Header'
 import { TodoList } from './components/Todos/TodoList'
 import { AddTodoForm } from './components/AddTodoForm/AddTodoForm'
 
+const todosLs = JSON.parse(localStorage.getItem('todos'))
+
 export const App = () => {
-  const [todos, setTodos] = useState([])
+  const [todos, setTodos] = useState(todosLs)
+  console.log('App ~ todos:', todos)
   const [searchTodo, setSearchTodo] = useState('')
   const [sortButton, setSortButton] = useState('all')
 
   const searchHandler = (event) => {
     setSearchTodo(event.target.value)
   }
+
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos))
+  }, [JSON.stringify(todos)])
 
   return (
     <div className='container mx-auto p-2'>
